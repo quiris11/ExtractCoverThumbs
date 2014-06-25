7c6cd0cde308f378337c0adc5d6e9ec14f7a44c1
Robert Błaut <listy@blaut.biz>
2014-06-25 20:43:05 +0200

    bump version number

--- __main__.py
+++ __main__.py
@@ -27,7 +27,7 @@ except ImportError:
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-V', '--version', action='version',
-                    version="%(prog)s (version 0.5)")
+                    version="%(prog)s (version 0.6)")
 parser.add_argument("kindle_directory", help="directory where is a Kindle"
                     " Paperwhite mounted")
 parser.add_argument("-v", "--verbose", help="print more informations",
